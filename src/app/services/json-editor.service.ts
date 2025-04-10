fbde12e1b2475392ad46f75710b65b4b178d9f23
Tanmay Sarkar <tanmay.sarkar@re-twin.energy>
2025-04-10 15:48:57 +0530

    Remove call to loadSampleData in JsonEditorService constructor to prevent loading sample data on initialization.

--- src/app/services/json-editor.service.ts
+++ src/app/services/json-editor.service.ts
@@ -12,7 +12,7 @@ export class JsonEditorService {
   private originalJson: any = null;
 
   constructor() {
-    this.loadSampleData();
+    // this.loadSampleData();
   }
 
   getTranslationObj() {
